f62d74d634d69cce41aa7bae9a4bcccd633a003e
lchambos <lchambos@f1br4s5.paris.42.school>
2026-05-13 15:59:28 +0200

    debut ex00

---
+++ ex00/fixed.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchambos <lchambos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/13 15:20:03 by lchambos          #+#    #+#             */
+/*   Updated: 2026/05/13 15:42:49 by lchambos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+  private:
+	int _value;
+	static const int _bits = 8;
+
+  public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+};
+
+#endif
